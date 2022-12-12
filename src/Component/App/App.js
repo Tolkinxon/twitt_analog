88d8b6f615d6e7886582f15da6cb86df7e78b561
Tolkinxon <krosstony639@gmail.com>
2022-12-12 23:59:17 +0500

    finished delete function

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -4,31 +4,40 @@ import PostStatusFilter from '../PostStatusFilter'
 import PostList from '../PostList'
 import PostAddForm from '../PostAddForm'
 import './App.css'
-import React from "react"
+import React from 'react'
 
-
-export default class App extends React.Component{
-
+export default class App extends React.Component {
   state = {
-     data: [
-      {label: "I need to learn VueJs", important: true, id: 'qa'},
-      {label: "I need to learn ReactJs", important: false, id: 'qb'},
-      {label: "I need to learn NodeJs", important: false, id: 'qf'},
-    ]
+    data: [
+      { label: 'I need to learn VueJs', important: true, id: 'qa' },
+      { label: 'I need to learn ReactJs', important: false, id: 'qb' },
+      { label: 'I need to learn NodeJs', important: false, id: 'qf' },
+    ],
   }
 
-  render (){
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id)
+  
+      // data.splice(index, 1)
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)]
+      return {
+        data: newArr,
+      }
+    })
+  }
+
+  render() {
     return (
-      <div className='app'>
-      <AppHeader />
-      <div className="search-panel d-flex">
-        <SearchPanel />
-        <PostStatusFilter />
+      <div className="app">
+        <AppHeader />
+        <div className="search-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList posts={this.state.data} onDelete={this.deleteItem} />
+        <PostAddForm />
       </div>
-      <PostList posts = {this.state.data} onDelete={(id) => {console.log(id)}}/>
-      <PostAddForm />
-    </div>
     )
   }
 }
-

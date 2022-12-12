dc39d4a0e88fbf072c7fbeb45780a94a3c09b16d
Tolkinxon <krosstony639@gmail.com>
2022-12-12 22:50:39 +0500

    css changed

--- src/Component/PostListItem/PostListItem.js
+++ src/Component/PostListItem/PostListItem.js
@@ -1,18 +1,46 @@
 import './PostListItem.css'
+import React from 'react'
 
-const PostListItem = ({label, important = false}) => {
+export default class PostListItem extends React.Component{
 
-  let classnames = "app-list-item d-flex justify-content-between"
-
-  if(important){
-    classnames += ' important'
+  state = {
+    important: false,
+    like: false
   }
 
-  return (
-    <div className={classnames}>
-      <span className="app-list-item-label ">{label}</span>
+  onImportant = () => {
+    this.setState({important: !this.state.important})
+
+  }
+
+  
+  onLike = () => {
+    this.setState({like: !this.state.like})
+
+  }
+
+  render() {
+    const {label} = this.props
+    const {important, like} = this.state
+
+    let classnames = "app-list-item d-flex justify-content-between"
+
+    if(important){
+      classnames += ' important'
+    }
+
+    if(like){
+      classnames += ' like'
+    }
+
+    return(
+      <div className={classnames}>
+      <span className="app-list-item-label " onClick={this.onLike}>{label}</span>
       <div className="d-flex justify-content-center align-items-center">
-        <button className="btn-star btn-sm">
+        <button 
+          type='button'
+          className="btn-star btn-sm"
+          onClick={this.onImportant}>
           <i className="fa fa-star"></i>
         </button>
         <button className="btn-trash btn-sm">
@@ -21,7 +49,9 @@ const PostListItem = ({label, important = false}) => {
         <i className="fas fa-heart"></i>
       </div>
     </div>
-  )
+
+    )
+  }
 }
 
-export default PostListItem
+
